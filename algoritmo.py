d5073f775c63011ab935b772985a13b8249da818
Projectyuuri07 <projectyuuri07@gmail.com>
2023-08-31 09:08:57 -0300

    Update

---
+++ algoritmo.py
@@ -0,0 +1,48 @@
+from classe import*
+
+def main():
+    tarefa = ToDoLIST()
+    contid = 0
+
+    while True:
+
+        try:
+
+            limpar()
+            print("--|ToDo List|--\n [1]-Adicionar uma tarefa\n [2]-Listar Tarefas\n [3]-Excluir Tarefas\n [4]-Sair")
+            opi = input("Digite a opção desejada>> ")
+            match opi:
+
+                case 1:
+                    limpar()
+                    print("--|INFORME A TAREFA|--\n\n")
+                    contid += 1
+                    indice = contid
+                    decricao = input(" Escreva a sua tarefa: ")
+                    print("\n\n--|TAREFA ADICIONADA COM SUCESSO|--")
+
+                    tarefa.adicionar_tarefa(indice,decricao)
+                    parar()
+                
+                case 2:
+                    limpar()
+                    print("--|LISTAR TAREFAS|--")
+                    tarefa.gettlistar_tarefas()
+                    parar()
+
+                case 3:
+                    pass
+
+                case 4:
+                    print("...Saindo do programa")
+                    parar()
+                    break
+                
+                case _:
+                    print("Opção invalida\n Escolha apenas as opções acima!")
+                    
+
+        except Exception as erro:
+            print("Valor invalido")
+            print(erro.__class__.__name__)
+            print("")
---
+++ classe.py
@@ -0,0 +1,27 @@
+import os
+
+def limpar():
+    os.system("cls")
+
+def parar():
+    os.system("pause")
+
+class ToDoLIST:
+    def __init__(self):
+        self.lista = {}
+
+    def adicionar_tarefa(self, indice, descricao: str,):
+        self.descricao = descricao
+        self.indice = indice
+        self.lista[self.indice] = [self.descricao]
+
+    def excluir_tarefa(self, indice: int):
+        pass
+
+
+    def gettlistar_tarefas(self):
+        for chave,valor in self.lista.items():
+            print(f"{chave}. {valor}\n")
+
+
+
